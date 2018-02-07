7a2679671e18ca51f5cf00f65adb15c3b49ad9da
kinreyli <kinreyli@users.noreply.github.com>
2018-02-07 19:36:11 +0800

    Update indexhub.py

--- src/indexhub.py
+++ src/indexhub.py
@@ -1,3 +1,15 @@
 
 
 # filename: indexhub.py
+import os
+import time
+import pandas as pd
+from initialize import GetParentDir
+
+
+def GetIndexValue():
+    refPath = os.path.join(GetParentDir(), 'ref', 'params.csv')
+    df = pd.read_csv(refPath).set_index('key')
+    timediff = int(time.time())-int(df['value']['indexTime'])
+    if timediff>3600:
+    
