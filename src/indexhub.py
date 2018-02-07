7e16078c2ce8bf36077384b2bce9c10c1633c15b
kinreyli <kinreyli@users.noreply.github.com>
2018-02-07 19:45:52 +0800

    Update indexhub.py

--- src/indexhub.py
+++ src/indexhub.py
@@ -12,4 +12,15 @@ def GetIndexValue():
     df = pd.read_csv(refPath).set_index('key')
     timediff = int(time.time())-int(df['value']['indexTime'])
     if timediff>3600:
+        indexPath = pd.read_csv(df['value']['indexPath'])
+        dfindex = pd.read_csv(indexPath)
+        indexTime = dfindex['timestamp'][len(dfindex)-1]
+        indexValue = int(dfindex['index'][len(dfindex)-1]*100)/100
+        df['value']['indexTime'] = indexTime
+        df['value']['indexValue'] = indexValue
+        df.to_csv(refPath)
+    else:
+        indexValue = df['value']['indexValue']
+    return indexValue
+    
     
