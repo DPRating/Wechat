cfae87ce762fe77935e91d96c8ab89096d8c625d
kinreyli <kinreyli@users.noreply.github.com>
2018-02-08 18:08:36 +0800

    Update indexhub.py

--- src/indexhub.py
+++ src/indexhub.py
@@ -10,32 +10,23 @@ from initialize import GetParentDir
 
 def GetIndexValue():
     refPath = os.path.join(GetParentDir(), 'ref', 'params.csv')
-    print refPath
     df = pd.read_csv(refPath).set_index('key')
-    print df
     timediff = int(time.time())-int(df['value']['indexTime'])
-    print df['value']['indexCodePath']
     '''
+    # The logic below has been tranferred to Dapao30 code
     if timediff>3600:
-        # os.system('python ' + df['value']['indexCodePath'])
-        print 'back to GetIndexValue'
+        os.system('python ' + df['value']['indexCodePath'])
         indexPath = pd.read_csv(df['value']['indexPath'])
-        print indexPath
         dfindex = pd.read_csv(indexPath)
-        print dfindex
         indexTime = dfindex['timestamp'][len(dfindex)-1]
-        print indexTime
         indexValue = round(dfindex['index'][len(dfindex)-1], 2)
-        print indexValue
         df['value']['indexTime'] = indexTime
         df['value']['indexValue'] = indexValue
-        print df
         df.to_csv(refPath)
     else:
         indexValue = df['value']['indexValue']
     '''
-    indexValue = df['value']['indexValue']
-    print "after index calc"
+    indexValue = round(df['value']['indexValue'], 2)
     return indexValue
     
     
