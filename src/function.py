ce47af974cbacd1e8f183ebd0065889155710b7a
kinreyli <kinreyli@users.noreply.github.com>
2018-02-10 17:48:40 +0800

    Update function.py

--- src/function.py
+++ src/function.py
@@ -12,3 +12,8 @@ def LoadParam():
     paramPath = os.path.join(parentDir, 'ref', 'param.json')
     with open(paramPath) as f:
         param = json.load(f)
+    token = param['token']
+    dapao30Time = param['dapao30Time']
+    dapao30Value = param['dapao30Value']
+    return parentDir, token, dapao30Time, dapao30Value
+    
