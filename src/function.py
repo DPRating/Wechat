13c04a98f976ea2923943ab91381b8667e7c0be6
kinreyli <kinreyli@users.noreply.github.com>
2018-02-10 17:50:28 +0800

    Update function.py

--- src/function.py
+++ src/function.py
@@ -4,16 +4,9 @@
 
 import os
 import sys
-import json
 
 
-def LoadParam():
-    parentDir = os.path.dirname(os.path.dirname(os.path.realpath(sys.argv[0])))
-    paramPath = os.path.join(parentDir, 'ref', 'param.json')
-    with open(paramPath) as f:
-        param = json.load(f)
-    token = param['token']
-    dapao30Time = param['dapao30Time']
-    dapao30Value = param['dapao30Value']
-    return parentDir, token, dapao30Time, dapao30Value
+def GetParentDir():
+    path = os.path.dirname(os.path.realpath(sys.argv[0]))
+    return os.path.dirname(path)
     
