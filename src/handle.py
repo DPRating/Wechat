72a624738d570dc3bb0bf325df9578a474b3ef72
kinreyli <kinreyli@users.noreply.github.com>
2018-02-10 19:03:01 +0800

    Update handle.py

--- src/handle.py
+++ src/handle.py
@@ -44,7 +44,7 @@ class Handle(object):
                 if recMsg.Content in ['大炮30', '大炮综指']:
                     toUser = recMsg.FromUserName
                     fromUser = recMsg.ToUserName
-                    content = GetDapao30() + u'\n回复「大炮综指详情」可获取相关文章'.encode('utf-8')
+                    content = GetDapao30() + u'回复 大炮综指详情 可获取相关文章'.encode('utf-8')
                     replyMsg = reply.TextMsg(toUser, fromUser, content)
                     return replyMsg.send()
                 else:
