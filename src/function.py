fe32b6c4e60faee17e0b2f9f30704dd6955fc826
kinreyli <kinreyli@users.noreply.github.com>
2018-02-10 18:13:19 +0800

    Update function.py

--- src/function.py
+++ src/function.py
@@ -9,10 +9,16 @@ import sys
 def GetParentDir():
     path = os.path.dirname(os.path.realpath(sys.argv[0]))
     return os.path.dirname(path)
+
+
+def GetToken():
+    paramPath = os.path.join(GetParentDir(), 'ref', 'param.json')
+    with open(paramPath) as f:
+        param = json.load(f)
     
 
 def GetDapao30():
-    paramPath = os.path.join(GetParentDir, 'ref', 'param.json')
+    paramPath = os.path.join(GetParentDir(), 'ref', 'param.json')
     with open(paramPath) as f:
         param = json.load(f)
     return param['dapao30Value']
