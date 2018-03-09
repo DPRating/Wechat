0b7542909a0a0ff0e43f871e4bb64ad1f421823b
kinreyli <kinreyli@users.noreply.github.com>
2018-03-09 12:13:06 +0800

    Update handle.py

--- src/handle.py
+++ src/handle.py
@@ -43,21 +43,21 @@ class Handle(object):
             if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                if recMsg.Content in ['大炮综指', '大炮30', 'Darpal30']:
+                if recMsg.Content in ['Darpal30', '大炮30', '大炮综指']:
                     str1 = u'当前指数：'.encode('utf-8') + str(GetDarpal30Value())
                     str2 = u'\n大炮综指是由市值前30的数字货币按市值加权得到的指数，'.encode('utf-8')
                     str3 = u'可以反映出市场整体行情，想进一步了解可回复「大炮综指详情」'.encode('utf-8')
                     content = str1 + str2 + str3
-                elif recMsg.Content in ['大炮综指详情', '大炮30详情']:
+                elif recMsg.Content in ['大炮综指详情']:
                     content = str(GetDarpal30Doc())
-                elif recMsg.Content in ['BVIX', '恐慌指数', '波动率指数']:
-                    str1 = u'当前指数：'.encode('utf-8') + str(GetBvixValue())
-                    str2 = u'\nBVIX恐慌指数是从比特币期权市场中提取出的未来30天隐含波动率，'.encode('utf-8')
-                    str3 = u'指数值越高意味着比特币未来价格的不确定性越大，'.encode('utf-8')
-                    str4 = u'想进一步了解可回复「BVIX详情」'.encode('utf-8')
+                elif recMsg.Content in ['Darpal20', '大炮20', '大炮精选']:
+                    str1 = u'当前指数：'.encode('utf-8') + str(GetDarpal20Value())
+                    str2 = u'\n大炮精选是根据我们的评级方法，'.encode('utf-8')
+                    str3 = u'从市值较高的数字货币中精选出的20个标的构建的市场指数，'.encode('utf-8')
+                    str4 = u'想进一步了解可回复「大炮精选详情」'.encode('utf-8')
                     content = str1 + str2 + str3 + str4
-                elif recMsg.Content in ['BVIX详情']:
-                    content = str(GetBvixDoc())
+                elif recMsg.Content in ['大炮精选详情']:
+                    content = str(GetDarpal20Doc())
                 else:
                     content = ''
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
